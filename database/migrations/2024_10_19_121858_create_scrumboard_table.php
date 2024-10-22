e398625cb1159d3bd1dd223cc041714df5a95ce5
Thomas Almekinders <96112844+ThomasAlmekinders@users.noreply.github.com>
2024-10-22 11:38:43 +0200

    Database tabellen voor scrumbord inhoud gemaakt.

--- database/migrations/2024_10_19_121858_create_scrumboard_table.php
+++ database/migrations/2024_10_19_121858_create_scrumboard_table.php
@@ -25,10 +25,33 @@ return new class extends Migration
             $table->timestamps();
             $table->unique(['user_id', 'scrumboard_id']);
         });
+
+        Schema::create('scrumboard_sprints', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('scrumboard_id')->references('id')->on('scrumboards')->onDelete('cascade')->name('scrumboard_sprint_scrumboard_id')->index();
+            $table->string('name');
+            $table->date('planned_start_date');
+            $table->date('planned_end_date');
+            $table->timestamps();
+        });
+
+        Schema::create('scrumboard_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sprint_id')->references('id')->on('scrumboard_sprints')->onDelete('cascade')->name('scrumboard_task_sprint_id')->index();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['to_do', 'in_progress', 'done'])->default('to_do');
+            $table->timestamp('finished_at')->nullable();
+            $table->timestamps();
+        });
     }
 
     public function down(): void
     {
+        Schema::dropIfExists('scrumboard_tasks');
+
+        Schema::dropIfExists('scrumboard_sprints');
+
         Schema::dropIfExists('scrumboard_user');
 
         Schema::dropIfExists('scrumboards');
